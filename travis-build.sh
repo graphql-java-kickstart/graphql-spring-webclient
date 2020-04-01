fda209213d5d9b4f1f3971ac7d941f6b0670af52
Michiel Oliemans <oliemans@scopisto.com>
2020-04-01 17:49:51 +0200

    Enabling snapshot builds again after bintray reconfiguration

--- travis-build.sh
+++ travis-build.sh
@@ -15,18 +15,15 @@ EOL
 }
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
-#  if [ "${RELEASE}" = "true" ]; then
+  if [ "${RELEASE}" = "true" ]; then
     echo "Deploying release to Bintray"
-#    NEXT_VERSION="$(gradle properties -q | grep "version:" | grep -v "kotlin_version:" | awk '{print $2}' | tr -d '[:space:]')b$(date +%Y%m%d%H%M)"
-    NEXT_VERSION="$(./gradlew properties -q | grep "version:" | grep -v "kotlin_version:" | awk '{print $2}' | tr -d '[:space:]')b${TRAVIS_BUILD_NUMBER}"
-    sed -i -E "s/^version(\s)?=.*/version=${NEXT_VERSION}/" gradle.properties
     saveGitCredentials
     ./gradlew clean assemble && ./gradlew check --info && ./gradlew bintrayUpload -x check --info
-#  else
-#    echo "Deploying snapshot"
-#    saveGitCredentials
-#    ./gradlew artifactoryPublish -Dsnapshot=true -Dbuild.number="${TRAVIS_BUILD_NUMBER}"
-#  fi
+  else
+    echo "Deploying snapshot"
+    saveGitCredentials
+    ./gradlew artifactoryPublish -Dsnapshot=true -Dbuild.number="${TRAVIS_BUILD_NUMBER}"
+  fi
 else
     echo "Verify"
     ./gradlew clean assemble && ./gradlew check --info
